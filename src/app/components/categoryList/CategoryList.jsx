a57649f3212aa6e871b5d9c962d169da9354c0b6
Vaibhavr699 <78257023+Vaibhavr699@users.noreply.github.com>
2024-09-29 21:17:05 +0530

    blog

---
+++ src/app/components/categoryList/CategoryList.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import styles from "./categoryList.module.css";
+import Link from "next/link";
+import Image from "next/image";
+
+
+const CategoryList = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Popular Categories</h1>
+      <div className={styles.categories}>
+        <Link href="/blog?cat=style" className={`${styles.category} ${styles.style}`}>
+              <Image
+                src="/style.png"
+                alt=""
+                width={32}
+                height={32}
+                className={styles.image}
+              />
+              style 
+          </Link>
+          <Link href={`/blog`} className={`${styles.category} ${styles.style}`}>
+          <Image
+            src="/fashion.png"
+            alt=""
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          fashion
+      </Link>
+      <Link href={`/blog`} className={`${styles.category} ${styles.style}`}>
+          <Image
+            src="/food.png"
+            alt=""
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          food
+      </Link>
+      <Link href={`/blog`} className={`${styles.category} ${styles.style}`}>
+          <Image
+            src="/travel.png"
+            alt=""
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          travel
+      </Link>
+      <Link href={`/blog`} className={`${styles.category} ${styles.style}`}>
+          <Image
+            src="/culture.png"
+            alt=""
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          culture
+      </Link>
+      <Link href={`/blog`} className={`${styles.category} ${styles.style}`}>
+          <Image
+            src="/coding.png"
+            alt=""
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          coding
+      </Link>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryList;
---
+++ src/utils/auth.js
@@ -0,0 +1,18 @@
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import prisma from "./connect";
+import { getServerSession } from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
+export const authOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    
+  ],
+};
+
+export const getAuthSession = () => getServerSession(authOptions);
